ef36053966cdad73d07e5ed8397715178bad4db8
c-o-m-o-n <comon928@gmail.com>
2025-01-12 20:38:41 +0300

    style landing page

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 
 function Navbar() {
   return (
-    <div className="fixed top-0 w-full flex justify-between p-4 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 shadow-lg border">
+    <div className="fixed top-0 w-full flex justify-between p-4 rounded-2xl bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 shadow-lg border border-[#3c3e44]">
       <div className='flex space-x-4'>
         <Image
           className="dark:invert"
@@ -21,29 +21,29 @@ function Navbar() {
 
        <a className='text-white p-2 rounded-3xl' href='#'>
 
-        <h1 className="flex items-center justify-center border rounded-3xl py-0 px-4 bg-gray-300 bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10" >Home</h1>
+        <h1 className="flex items-center justify-center border border-[#3c3e44] rounded-3xl py-0 px-4 bg-gray-300 bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10" >Home</h1>
        </a>
        <a className='text-white p-2 rounded-3xl' href='#'>
-        <h1 className="flex items-center justify-center border rounded-3xl py-0 px-4 bg-gray-300 bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10" >About</h1>
+        <h1 className="flex items-center justify-center border border-[#3c3e44] rounded-3xl py-0 px-4 bg-gray-300 bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10" >About</h1>
        </a>
        <a className='text-white p-2 rounded-3xl' href='#'>
-        <h1 className="flex items-center justify-center border rounded-3xl py-0 px-4 bg-gray-300 bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10" >Members</h1>
+        <h1 className="flex items-center justify-center border border-[#3c3e44] rounded-3xl py-0 px-4 bg-gray-300 bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10" >Members</h1>
        </a>
        <a className='text-white p-2 rounded-3xl' href='#'>
-        <h1 className="flex items-center justify-center border rounded-3xl py-0 px-4 bg-gray-300 bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10" >Portfolio pages</h1>
+        <h1 className="flex items-center justify-center border border-[#3c3e44] rounded-3xl py-0 px-4 bg-gray-300 bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10" >Portfolio pages</h1>
        </a>
        <a className='text-white p-2 rounded-3xl' href='#'>
-        <h1 className="flex items-center justify-center border rounded-3xl py-0 px-4 bg-gray-300 bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10" >News</h1>
+        <h1 className="flex items-center justify-center border border-[#3c3e44] rounded-3xl py-0 px-4 bg-gray-300 bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10" >News</h1>
        </a>
        <a className='text-white p-2 rounded-3xl' href='#'>
-        <h1 className="flex items-center justify-center border rounded-3xl py-0 px-4 bg-gray-300 bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10" >Resources</h1>
+        <h1 className="flex items-center justify-center border border-[#3c3e44] rounded-3xl py-0 px-4 bg-gray-300 bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10" >Resources</h1>
        </a>
        <a className='text-white p-2 rounded-3xl' href='#'>
-        <h1 className="flex items-center justify-center border rounded-3xl py-0 px-4 bg-gray-300 bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10" >Contact</h1>
+        <h1 className="flex items-center justify-center border border-[#3c3e44] rounded-3xl py-0 px-4 bg-gray-300 bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10" >Contact</h1>
        </a>
       </div>
-      <a className='text-white p-2 rounded-3xl' href='#'>
-        <h1 className="flex items-center justify-center border rounded-3xl py-0 px-4 bg-gray-300 bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10" >Join server</h1>
+      <a className='text-white p-2 rounded-3xl' href='https://discord.gg/eqSU46Y7xW'>
+        <h1 className="flex items-center justify-center border border-[#3c3e44] rounded-3xl py-0 px-4 bg-gray-300 bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10" >Join server</h1>
       </a>
     </div>
   )
