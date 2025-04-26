6c8c83c2d7c678f2df13b54dac68b1672d45a37c
C-o-m-o-n <comon928@gmail.com>
2025-04-26 15:27:24 +0300

    add seo

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,6 +27,41 @@ export default function RootLayout({
   return (
     <html lang="en">
       <GoogleTagManager gtmId="G-2LBDSNVRW7" />
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name="description"
+          content="👋 Hey! 👋 Looking for a chill spot to hang out, make friends, and level up your skills? 😎 Comon is the place to be! 🎉 We're a community for everyone, from coding wizards to casual chatters! 💖
+👥 Meet awesome people, share crazy ideas, and make new friends!
+💻 Geek out about code, games, anime, and everything in between! 🤓
+👧 Chat with Activa, our friendly AI buddy! 🥰 She's here for witty banter, deep conversations, and all-around fun! ✨
+💬 All are welcome! Bring your unique self and join the fun! ✨
+Join Comon today and let's create something amazing... together! 🥰 "
+        />
+        <meta
+          name="keywords"
+          content="Comon, community, server, collaboration"
+        />
+        <meta name="author" content="Comon Team" />
+        <meta property="og:title" content="Comon" />
+        <meta
+          property="og:description"
+          content="👋 Hey! 👋 Looking for a chill spot to hang out, make friends, and level up your skills? 😎 Comon is the place to be! 🎉 We're a community for everyone, from coding wizards to casual chatters! 💖
+👥 Meet awesome people, share crazy ideas, and make new friends!
+💻 Geek out about code, games, anime, and everything in between! 🤓
+👧 Chat with Activa, our friendly AI buddy! 🥰 She's here for witty banter, deep conversations, and all-around fun! ✨
+💬 All are welcome! Bring your unique self and join the fun! ✨
+Join Comon today and let's create something amazing... together! 🥰 "
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://www.comon.tech" />
+        <meta property="og:image" content="https://www.comon.tech/icon.png" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Comon" />
+        <meta name="twitter:description" content="Join the Comon server" />
+        <meta name="twitter:image" content="https://www.comon.tech/icon.png" />
+        <link rel="canonical" href="https://www.comon.tech" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-background text-foreground dark`}
       >
